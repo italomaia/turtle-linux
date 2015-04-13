c0a2bd0d7205076ae016fa740e05bc759834cee3
Italo Maia <italo.maia@gmail.com>
2015-04-13 20:09:05 -0300

    refactored setup.sh

--- setup/1404/32bits/setup.sh
+++ setup/1404/32bits/setup.sh
@@ -14,12 +14,14 @@ apt-get purge -y mousepad  # medit
 apt-get purge -y gnome-mines   # because ...
 apt-get purge -y gnome-sudoku  # because ...
 
+apt-get autoremove -y
+
 # editors
 apt-get install -y ne medit editra
 
 # ftp client
-apt-get install -y foff \  # ftp client
-midori  # browser
+apt-get install -y foff  # ftp client
+apt-get install -y midori  # browser
 
 # media
 apt-get install -y quodlibet mplayer2
@@ -29,10 +31,11 @@ apt-get install -y build-essential python-dev python-pip
 apt-get install -y libssl-dev libxml2-dev libxslt1-dev libssl-dev libffi-dev libpcre3-dev
 
 # dev tools
-apt-get install -y ipython bpython \
-kiki \  # regex tester
-git-core  \  # vcs
-git-cola   # git manager
+apt-get install -y ipython bpython
+# regex tester
+apt-get install -y kiki
+# vcs
+apt-get install -y git-core git-cola
 
 # for gamers
 apt-get install -y python-pygame
@@ -43,8 +46,6 @@ apt-get install -y pcsxr mupen64plus desmume
 pip install virtualenvwrapper==4.3.2
 pip install numpy==1.8.2
 pip install PyOpenGL-accelerate==3.1
-
-apt-get autoremove -y
 
 dpkg-divert --local --add /etc/init.d/systemd-logind
 ln -s /bin/true /etc/init.d/systemd-logind
